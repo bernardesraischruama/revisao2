af480ac7d887ab8e44a5e1d135d3f456f36ed6f9
bernardesraischruama <bernardesraichruama@gmail.com>
2024-07-17 09:08:46 -0300

    revisao

---
+++ src/views/EquipeView.vue
@@ -0,0 +1,39 @@
+<script>
+export default {
+    name: 'EquipeView',
+    data() {
+        return {
+            equipe: [
+                {
+                    nome: 'Juliane',
+                    cargo: 'Desenvolvedor'
+                },
+                {
+                    nome: 'Ruama',
+                    cargo: 'Designer'
+                },
+                {
+                    nome: 'Ester',
+                    cargo: 'Gerente de Projetos'
+                }
+            ]
+        }
+    }
+}
+</script>
+
+<template>
+    <div>
+        <h1>Equipe</h1>
+        <ul>
+            <li v-for="membro in equipe" :key="membro.nome">
+                <h2>{{ membro.nome }}</h2>
+                <p>{{ membro.cargo }}</p>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<style scoped>
+/* Your component-specific styles go here */
+</style>
